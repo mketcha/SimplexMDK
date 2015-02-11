bffe19bcbd37e3a1d57f45b9815b921fc16c1723
mketcha <mketcha@gmail.com>
2015-02-11 15:01:10 -0500

    Fixed error with clustering

--- Code/runDrchSMBClusterCompare.R
+++ Code/runDrchSMBClusterCompare.R
@@ -14,14 +14,15 @@ source("generateDirilechtSBM.R");
 B <- matrix(c(.42, .42, .42,.5),2);
 PI <- c( .5, .5);
 n <- 100;
-m <- 100;
-trials <- 1000
+m <- 1;
+trials <- 1000;
 max_k <- 5;
 r <- 20;
 nclust <- 10
 myoptions <- igraph.arpack.default
 myoptions$maxiter <- 20000
 
+error_Phat_noClust <- matrix(0, trials, max_k);
 error_Phat <- matrix(0, trials, max_k);
 error_mean <- matrix(0, trials, 1);
 BASE <- generateDirilechtBASE(B, myoptions)
@@ -49,14 +50,27 @@ for (i in 1:trials) {
     clust = matrix(0,n,j)
     if (is.vector(cl$parameters$mean))
       cl$parameters$mean <- matrix(cl$parameters$mean, ncol = nclust)
-    for (cent in 1:nclust)
-      clust[cl$classification==cent,] <- cl$parameters$mean[,cent];
-    #P_hat <- ASE$X %*% t(ASE$X);
+    for (cent in 1:nclust){
+      if (sum(cl$classification==cent)!=0)
+        clust[cl$classification==cent,] <- matrix(rep(t(cl$parameters$mean[,cent, drop = FALSE]),sum(cl$classification==cent)), nrow = sum(cl$classification==cent), byrow=TRUE);
+    }
+    P_hat_noClust <- ASE$X %*% t(ASE$X);
+    err_v_noClust <- P-P_hat_noClust;
+    err_v_noClust <- err_v_noClust[upper.tri(err_v_noClust)];
+    error_Phat_noClust[i,j] <- mean(err_v_noClust^2);
+    
     P_hat <- clust %*% t(clust)
     err_v <- P-P_hat;
     err_v <- err_v[upper.tri(err_v)];
     error_Phat[i,j] <- mean(err_v^2);;
   }
 }
-#vioplot( error_mean, error_Phat[,1], error_Phat[,2],error_Phat[,3], error_Phat[,4], error_Phat[,5], names = c("Naive Mean", "k = 1", "k = 2", "k = 3", "k = 4", "k = 5"));
-#title("M = 1, N = 100, r = 20, clusters = 10")
+vioplot( error_mean, error_Phat_noClust[,1], error_Phat[,1], error_Phat_noClust[,2], error_Phat[,2], error_Phat_noClust[,3], error_Phat[,3], error_Phat_noClust[,4], error_Phat[,4], error_Phat_noClust[,5], error_Phat[,5], names = c("Naive", "k=1", "k=1", "k=2", "k=2", "k=3", "k=3","k=4", "k=4","k=5", "k=5"), col =c("blue"));
+vioplot(error_mean, at = 1, col = c("magenta"), add= TRUE)
+vioplot(error_Phat_noClust[,1], at = 2, col = c("green"), add= TRUE)
+vioplot(error_Phat_noClust[,2], at = 4, col = c("green"), add= TRUE)
+vioplot(error_Phat_noClust[,3], at = 6, col = c("green"), add= TRUE)
+vioplot(error_Phat_noClust[,4], at = 8, col = c("green"), add= TRUE)
+vioplot(error_Phat_noClust[,5], at = 10, col = c("green"), add= TRUE)
+title("M = 1, N = 100, r = 20, clusters = 10")
+#legend(locator(1), legend= c("Naive", "X*X'", "Up to 10 clusters"), fill = c("magenta","green", "blue"))
